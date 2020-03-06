c325e42d673ac7d8145e91a707cf8e34bd2f4364
Gustavo <gugurock50@gmail.com>
2020-03-05 22:50:42 -0300

    fixed readme

--- configure.php
+++ configure.php
@@ -1,15 +1,27 @@
-DROP TABLE IF EXISTS messages;
-CREATE TABLE messages(
+<?php
+
+include 'dbService.php';
+
+$databaseService = new DatabaseService();
+$conn = $databaseService->getConnection();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS messages");
+$stmt->execute();
+
+$stmt = $conn->prepare("CREATE TABLE messages(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     emitter VARCHAR(1024) NOT NULL,
     receiving VARCHAR(1024) NOT NULL,
     message longtext NOT NULL,
     visibleAt timestamp NOT NULL DEFAULT NOW(),
     createdAt timestamp NOT NULL DEFAULT NOW()
-);
+)");
+$stmt->execute();
 
-DROP TABLE IF EXISTS nodes;
-CREATE TABLE nodes(
+$stmt = $conn->prepare("DROP TABLE IF EXISTS nodes");
+$stmt->execute();
+
+$stmt = $conn->prepare("CREATE TABLE nodes(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     address VARCHAR(256) NOT NULL,
@@ -18,4 +30,5 @@ CREATE TABLE nodes(
     lastFail timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     updatedAt timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     createdAt timestamp NOT NULL DEFAULT NOW()
-);
+)");
+$stmt->execute();
